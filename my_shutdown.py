d191bba55bdb25b78cf5608b0b31682b2240f41c
bel <aabelyakov@mail.ru>
2023-08-27 20:24:58 +0300

    q

--- my_shutdown.py
+++ my_shutdown.py
@@ -17,8 +17,7 @@ def shutdown():
     # процессу сигнал останова TERMINATE
     os.kill(os.getpid(), signal.SIGTERM)
     return {"Прощание": "Процесс сервера GUVICORN остановлен"}
-#enddef
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-#endif
