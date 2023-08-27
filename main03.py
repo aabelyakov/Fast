b79ac68d06821887af86e88560b00bf18f6e988e
bel <aabelyakov@mail.ru>
2023-08-27 12:44:28 +0300

    q

--- main03.py
+++ main03.py
@@ -16,8 +16,8 @@ async def root():
 # uvicorn main:app --reload
 
 # или добавить в текст программы эту конструкцию
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
     # Uvicorn running on http://127.0.0.1:8000 (Press CTRL+C to quit)
 # endif
 
